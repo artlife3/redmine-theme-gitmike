34f4b3ddbd6412ce8b5889b41d5a3bf6daab20b9
Atsushi Iwasaki <iwasaki@grab-desgin.com>
2014-03-26 03:37:28 +0900

    #4

--- javascripts/theme.js
+++ javascripts/theme.js
@@ -73,10 +73,11 @@ $(function()
 	/**
 	 * issues history
 	 */
-	$(".journal.has-details").not(".has-notes").wrap('<div class="balloon-left"></div>');
-	$(".journal.has-notes").wrap('<div class="balloon-right"></div>');
+	$(".journal.has-details").not(".has-notes").wrap('<div class="balloon"><div class="balloon-left"></div></div>');
+	$(".journal.has-notes").wrap('<div class="balloon"><div class="balloon-right"></div></div>');
 
 	$('<a id="history_btn">show detail</a>').appendTo(".action-show #history h3");
+	$('<a id="issue-changesets_btn">show detail</a>').appendTo("#issue-changesets h3");
 	$(".balloon-left").hide();
 	$("#history_btn").addClass("history-detail-hide");
 	$("#history_btn").click(
@@ -91,6 +92,18 @@ $(function()
 				$(this).addClass("history-detail-hide").fadeTo("normal", 1);
 			}
 	});
+	// $("#issue-changesets_btn").click(
+		// function(){
+			// if($(this).hasClass("history-detail-hide")){
+				// $(".balloon-left").stop(true, false).
+				// slideDown("normal","easeOutQuint");
+				// $(this).removeClass("history-detail-hide").fadeTo("normal", 0.3);
+			// }else{
+				// $(".balloon-left").stop(true, false).
+				// slideUp("normal","easeOutQuint");;
+				// $(this).addClass("history-detail-hide").fadeTo("normal", 1);
+			// }
+	// });
 });
 
 
